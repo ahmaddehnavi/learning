6fbe3858799abc8d61c37e5c658886decc9c0baa
Ahmad <ahmad.829@gmail.com>
2013-08-01 19:52:24 +0430

    add dashboard view

---
+++ application/views/user/dashboard.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+    <!-- global code  -->
+    <meta charset="UTF-8"/>
+    <meta name="author" content=""/>
+    <meta name='keywords' content=''/>
+    <meta name='description' content=''/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="HandheldFriendly" content="true"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no"/>
+
+    <!--[if lt I E 9]>
+    <script src="<?=FILES_JS_PATH?>/base/html5shiv.js"></script>
+    <script src="<?=FILES_JS_PATH?>/base/css3-mediaqueries.js"></script>
+    <![end if]-->
+
+    <link rel="stylesheet" type="text/css" href="<?= FILES_CSS_PATH ?>/base/global.css"/>
+    <!-- ############################################################################ -->
+
+    <link rel="stylesheet" type="text/css" href="<?= FILES_CSS_PATH ?>/style1.css"/>
+</head>
+
+<body>
+
+<div id="container">
+    <div id="content">
+        <header>
+            panda academy
+        </header>
+        <main>
+            content content content content content content content content content content content content content
+        </main>
+    </div>
+    <aside id="sidebar">
+        <header>ahmad dehnavi</header>
+        <nav>
+            <menu>
+                <li><a href="#">item 1</a></li>
+                <li><a href="#">item 2</a></li>
+                <li><a href="#">item 3</a></li>
+                <li><a href="#">item 4</a></li>
+                <li><a href="#">item 5</a></li>
+            </menu>
+        </nav>
+    </aside>
+    <footer id="footer">footer</footer>
+</div>
+
+
+<!-- ############################################################################ -->
+<script src="<?= FILES_JS_PATH ?>/base/prefixfree.min.js"></script>
+<!--[if lt IE 9]>
+<script src="<?=FILES_JS_PATH?>/base/jquery-1.9.0.js"></script>
+<![endif]-->
+<!--[if gte IE 9]><!-->
+<script src="<?= FILES_JS_PATH ?>/base/jquery-2.0.3.js"></script>
+<!--[endif]-->
+<!-- ############################################################################ -->
+
+
+<script type="text/javascript" src="<?= FILES_JS_PATH ?>/slider.js"></script>
+
+</body>
+</html>
