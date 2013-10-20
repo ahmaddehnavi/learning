9231265bbee8a18e0f4d76aba42c60b68607ab5f
Ahmad <ahmad.829@gmail.com>
2013-10-20 17:45:34 +0330

    ahmad

--- application/views/user/password_send.php
+++ application/views/user/password_send.php
@@ -1,12 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Password Reset Form</title>
+<?php $this->load->view('base/header',array('title'=>'new password sent')); ?>
+<style type="text/css">
+	#message {
+		text-align: center;
+		margin-top: 100px;
+		background-color: #00aa00;
+		color: #fff;
+		padding: 20px 5px;
+	}
+</style>
+<nav>
+	<menu>
+		<li><?= anchor('/','home')?></li>
+		<li><?=anchor('/academy','academy')?></li>
+		<li class="active"><?=anchor('/user','user')?></li>
+		<li><a href="<?=FILE_MANAGE_PATH?>">file management</a></li>
+		<div class="badboy"></div>
+	</menu>
+	<ul class="collapse-btn btn-top toggle-on" collapse-target="nav">
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</nav>
 
-</head>
-<body>
+<div id="message">
+	new your password sent to mail.</br>
+	please check your mail</br></br>
+	<?=anchor('user/login','click here ')?> and login with new password.
+</div>
 
- new your password sent to mail.
 </body>
 </html>
--- application/views/user/reset_requested.php
+++ application/views/user/reset_requested.php
@@ -1,13 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Reset Requested</title>
+<?php $this->load->view('base/header',array('title'=>'reset password sent')); ?>
+<style type="text/css">
+	#message {
+		text-align: center;
+		margin-top: 100px;
+		background-color: #00aa00;
+		color: #fff;
+		padding: 20px 5px;
+	}
+</style>
+<nav>
+	<menu>
+		<li><?= anchor('/','home')?></li>
+		<li><?=anchor('/academy','academy')?></li>
+		<li class="active"><?=anchor('/user','user')?></li>
+		<li><a href="<?=FILE_MANAGE_PATH?>">file management</a></li>
+		<div class="badboy"></div>
+	</menu>
+	<ul class="collapse-btn btn-top toggle-on" collapse-target="nav">
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</nav>
 
-</head>
-<body>
-
-<p>Check your email for instructions on how to reset your password.</p>
+<div id="message">
+	Check your email for instructions on how to reset your password.
+</div>
 
 </body>
 </html>
