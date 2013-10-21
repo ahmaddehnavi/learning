4aee3501fb636174ef3a07d1335d654c9b4ebe43
Ahmad <ahmad.829@gmail.com>
2013-10-21 23:59:28 +0330

    ahmad

--- application/views/user/messages/conversation.php
+++ application/views/user/messages/conversation.php
@@ -16,7 +16,8 @@
 			</ul>
 		</nav>
 		<aside id="sidebar">
-			<header><?php echo $this->auth->get_full_name(); ?>&nbsp;</header>
+			<header><img src="<?= FILES_USERS_PATH . '/' . $this->auth->get_user_id() ?>/image/profile_80.jpg"
+						 width="40px" height="40px" id="user-image"/><?php echo $this->auth->get_full_name(); ?>&nbsp;</header>
 			<ul class="collapse-btn btn-left toggle-off" collapse-target="#sidebar">
 				<li></li>
 				<li></li>
